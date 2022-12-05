2e37b15c18433df45e1a4eb62b66b94f44ab6f11
yo16 <xxyo1xx@gmail.com>
2022-12-06 00:24:23 +0900

    input box

--- vscode-omikuji-yo16/src/extension.ts
+++ vscode-omikuji-yo16/src/extension.ts
@@ -2,12 +2,25 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('vscode-omikuji-yo16.openOmikuji', () => {
-		const omikujiCandidates = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶'];
-		const omikujiResult = omikujiCandidates[Math.floor(Math.random()*omikujiCandidates.length)];
+	let disposable = vscode.commands.registerCommand(
+		'vscode-omikuji-yo16.openOmikuji',
+		async () => {
+			const omikujiCandidates = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶'];
+			const omikujiResult = omikujiCandidates[
+				Math.floor(Math.random()*omikujiCandidates.length)
+			];
+			const your_name = await vscode.window.showInputBox({
+				title: 'あなたの名前は？',
+				value: '太郎'
+			});
 
-		vscode.window.showInformationMessage(`あなたの運勢は${omikujiResult}です！！`);
-	});
+			if (your_name !== undefined){
+				vscode.window.showInformationMessage(`${your_name}さんの運勢は${omikujiResult}です！！`);
+			} else {
+				vscode.window.showInformationMessage(`あなたの運勢は${omikujiResult}です！！`);
+			}
+		}
+	);
 	context.subscriptions.push(disposable);
 
 	
